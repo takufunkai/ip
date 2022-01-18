d904dc48c3da157c62431e28dd9ca01c5633a714
takufunkai <takufunkai@gmail.com>
2022-01-18 19:36:42 +0800

    Add list functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -10,34 +12,43 @@ public class Duke {
 
     private final static String MESSAGE_BUFFER = "                 ";
     private final static String DIVIDER =
-        " ---------------  ---------------  ---------------  ---------------  --------------- \n" +
-        " -:::::::::::::-  -:::::::::::::-  -:::::::::::::-  -:::::::::::::-  -:::::::::::::- \n" +
-        " ---------------  ---------------  ---------------  ---------------  --------------- \n";
+            " ---------------  ---------------  ---------------  ---------------  --------------- \n" +
+                    " -:::::::::::::-  -:::::::::::::-  -:::::::::::::-  -:::::::::::::-  -:::::::::::::- \n" +
+                    " ---------------  ---------------  ---------------  ---------------  --------------- \n";
     private final static String GREETING_MESSAGE =
-        wrapStringRed(
-    "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⣤⣤⣤⣤⣶⣦⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
-        "⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⡿⠛⠉⠙⠛⠛⠛⠛⠻⢿⣿⣷⣤⡀⠀⠀⠀⠀⠀\n" +
-        "⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⠋⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⠈⢻⣿⣿⡄⠀⠀⠀⠀\n" +
-        "⠀⠀⠀⠀⠀⠀⠀⣸⣿⡏⠀⠀⠀⣠⣶⣾⣿⣿⣿⠿⠿⠿⢿⣿⣿⣿⣄⠀⠀⠀\n" +
-        "⠀⠀⠀⠀⠀⠀⠀⣿⣿⠁⠀⠀⢰⣿⣿⣯⠁⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣷⡄⠀\n" +
-        "⠀⠀⣀⣤⣴⣶⣶⣿⡟⠀⠀⠀⢸⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣷⠀\n" +
-        "⠀⢰⣿⡟⠋⠉⣹⣿⡇⠀⠀⠀⠘⣿⣿⣿⣿⣷⣦⣤⣤⣤⣶⣶⣶⣶⣿⣿⣿⠀\n" +
-        "⠀⢸⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀\n" +
-        "⠀⣸⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠉⠻⠿⣿⣿⣿⣿⡿⠿⠿⠛⢻⣿⡇⠀⠀        ඞ Hello, I am Red from Among Us.\n" +
-        "⠀⣿⣿⠁⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣧⠀\n" +
-        "⠀⣿⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀            ඞ We are currently facing a crisis onboard --\n" +
-        "⠀⣿⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀⠀          -- there seems to be an imposter among us...\n" +
-        "⠀⢿⣿⡆⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀\n" +
-        "⠀⠸⣿⣧⡀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠃⠀⠀          ඞ My job is to handle chat requests,\n" +
-        "⠀⠀⠛⢿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⣰⣿⣿⣷⣶⣶⣶⣶⠶⠀⢠⣿⣿⠀⠀⠀        so although I might get murdered any moment now...\n" +
-        "⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⣽⣿⡏⠁⠀⠀⢸⣿⡇⠀⠀⠀            ... how can I help you?\n" +
-        "⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⢹⣿⡆⠀⠀⠀⣸⣿⠇⠀⠀⠀\n" +
-        "⠀⠀⠀⠀⠀⠀⠀⢿⣿⣦⣄⣀⣠⣴⣿⣿⠁⠀⠈⠻⣿⣿⣿⣿⡿⠏⠀⠀⠀⠀\n" +
-        "⠀⠀⠀⠀⠀⠀⠀⠈⠛⠻⠿⠿⠿⠿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n") +
-        DIVIDER;
+            wrapStringRed(
+                    "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⣤⣤⣤⣤⣶⣦⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+                            "⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⡿⠛⠉⠙⠛⠛⠛⠛⠻⢿⣿⣷⣤⡀⠀⠀⠀⠀⠀\n" +
+                            "⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⠋⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⠈⢻⣿⣿⡄⠀⠀⠀⠀\n" +
+                            "⠀⠀⠀⠀⠀⠀⠀⣸⣿⡏⠀⠀⠀⣠⣶⣾⣿⣿⣿⠿⠿⠿⢿⣿⣿⣿⣄⠀⠀⠀\n" +
+                            "⠀⠀⠀⠀⠀⠀⠀⣿⣿⠁⠀⠀⢰⣿⣿⣯⠁⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣷⡄⠀\n" +
+                            "⠀⠀⣀⣤⣴⣶⣶⣿⡟⠀⠀⠀⢸⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣷⠀\n" +
+                            "⠀⢰⣿⡟⠋⠉⣹⣿⡇⠀⠀⠀⠘⣿⣿⣿⣿⣷⣦⣤⣤⣤⣶⣶⣶⣶⣿⣿⣿⠀\n" +
+                            "⠀⢸⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀\n" +
+                            "⠀⣸⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠉⠻⠿⣿⣿⣿⣿⡿⠿⠿⠛⢻⣿⡇⠀⠀        " +
+                            "ඞ Hello, I am Red from Among Us.\n" +
+                            "⠀⣿⣿⠁⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣧⠀\n" +
+                            "⠀⣿⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀           " +
+                            "ඞ We are currently facing a crisis onboard --\n" +
+                            "⠀⣿⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀⠀          " +
+                            "-- there seems to be an imposter among us...\n" +
+                            "⠀⢿⣿⡆⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀\n" +
+                            "⠀⠸⣿⣧⡀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠃⠀⠀          " +
+                            "ඞ My job is to handle chat requests,\n" +
+                            "⠀⠀⠛⢿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⣰⣿⣿⣷⣶⣶⣶⣶⠶⠀⢠⣿⣿⠀⠀⠀        " +
+                            "so although I might get murdered any moment now...\n" +
+                            "⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⣽⣿⡏⠁⠀⠀⢸⣿⡇⠀⠀⠀            " +
+                            "... how can I help you?\n" +
+                            "⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⢹⣿⡆⠀⠀⠀⣸⣿⠇⠀⠀⠀\n" +
+                            "⠀⠀⠀⠀⠀⠀⠀⢿⣿⣦⣄⣀⣠⣴⣿⣿⠁⠀⠈⠻⣿⣿⣿⣿⡿⠏⠀⠀⠀⠀\n" +
+                            "⠀⠀⠀⠀⠀⠀⠀⠈⠛⠻⠿⠿⠿⠿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n") +
+                    DIVIDER;
 
     private final static String EXIT_COMMAND = "bye";
+    private final static String LIST_COMMAND = "list";
     /* END CONSTANTS */
+
+    private final static List<String> userCommands = new ArrayList<>(100);
 
     /* HELPER FUNCTIONS */
     private static String wrapStringRed(String out) {
@@ -51,43 +62,57 @@ public class Duke {
 
     private static void printFromRed(String message) {
         String redMessageSuffix = MESSAGE_BUFFER + " >>> " + CREW_MATE_LOGO + " > ";
-        System.out.println(wrapStringRed(redMessageSuffix + message + "\n"));
+        System.out.println(wrapStringRed(redMessageSuffix + message));
     }
 
-    private static void printInputMessage() {
-        System.out.print(MESSAGE_BUFFER + " >>> " + SMILEY_LOGO + " YOU > ");
-    }
-
-    private static void handleUserExit() {
+    private static void printExitMessage() {
         printFromRed("Thank you for chatting with me... bye forever");
         System.out.println(DIVIDER +
-            ". 　　　。　　　　•　 　ﾟ　　。 　　.\n" +
-            "\n" +
-            "　　　.　　　 　　.　　　　　。　　 。　. 　\n" +
-            "\n" +
-            ".　　 。　　　　　 "+wrapStringRed(CREW_MATE_LOGO)+" 。 . 　　 • 　　　　•\n" +
-            "\n" +
-            "　　ﾟ　　 Red was not An Impostor.　 。　.\n" +
-            "\n" +
-            "　　'　　　     2 Impostors remain 　 　　。\n" +
-            "\n" +
-            "　　ﾟ　　　.　　　. ,　　　　.　 .");
+                ". 　　　。　　　　•　 　ﾟ　　。 　　.\n" +
+                "\n" +
+                "　　　.　　　 　　.　　　　　。　　 。　. 　\n" +
+                "\n" +
+                ".　　 。　　　　　 " + wrapStringRed(CREW_MATE_LOGO) + " 。 . 　　 • 　　　　•\n" +
+                "\n" +
+                "　　ﾟ　　 Red was not An Impostor.　 。　.\n" +
+                "\n" +
+                "　　'　　　     2 Impostors remain 　 　　。\n" +
+                "\n" +
+                "　　ﾟ　　　.　　　. ,　　　　.　 .");
+    }
+
+    private static String awaitCommandFromUser(Scanner sc) {
+        System.out.print(MESSAGE_BUFFER + " >>> " + SMILEY_LOGO + " YOU > ");
+        return sc.nextLine();
+    }
+
+    private static void storeUserCommand(String command) {
+        userCommands.add(command);
+    }
+
+    private static void listUserCommands() {
+        for (int i = 0; i < userCommands.size(); i++) {
+            printFromRed((i + 1) + ". " + userCommands.get(i));
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         printGreeting();
-
-        String userInput = "";
         while (true) {
-            printInputMessage();
-            userInput = sc.nextLine();
+            String userInput = awaitCommandFromUser(sc);
             if (userInput.equalsIgnoreCase(EXIT_COMMAND)) {
-                handleUserExit();
+                printExitMessage();
                 break;
             }
-            printFromRed(userInput);
+            if (userInput.equalsIgnoreCase(LIST_COMMAND)) {
+                listUserCommands();
+                continue;
+            }
+            storeUserCommand(userInput);
+            printFromRed("added: " + userInput + "\n");
         }
         sc.close();
     }
