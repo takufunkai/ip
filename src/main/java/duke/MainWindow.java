c6118aca7ca52e245d756fc77c03a298b0abd44f
takufunkai <takufunkai@gmail.com>
2022-02-02 17:16:32 +0800

    Add correct display pictures

--- src/main/java/duke/MainWindow.java
+++ src/main/java/duke/MainWindow.java
@@ -23,8 +23,10 @@ public class MainWindow extends AnchorPane {
 
     private Duke duke;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/travis_scott.jpg"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/travis_scott_fish.jpg"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/red_default.png"));
+    private Image dukeImageError = new Image(this.getClass().getResourceAsStream("/images/red_shocked.png"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/user.png"));
+
 
     public MainWindow() {
         try {
